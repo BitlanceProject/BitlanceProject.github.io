11360d657fc770678b637a10e0d1648a5530be85
Vo <vo@unvoo.com>
2017-02-13 08:12:55 +0200

    website

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var $window = $(window);
+var $header = $("#header");
+var $logo = $(".splashPage .logoSection");
+
+function scrollTo(element, time){
+    if(!time) time = 300;
+    $('html, body').animate({
+        scrollTop: $(element).offset().top
+    }, time);
+}
+
+function initMenuRef(button, ref){
+    $(button).click(function(){
+        scrollTo(ref);
+    });
+}
+
+function stickyTop() {
+    //$header.toggleClass("scrolled",($window.height()-$header.outerHeight()<=$window.scrollTop()));
+    $header.toggleClass("scrolled",($window.height()-500<=$window.scrollTop()));
+    $logo.toggleClass("scrolled",($window.height()-500<=$window.scrollTop()));
+    $logo.css("margin-top",$window.scrollTop()/2);
+}
+
+
+
+$(document).ready(function(){
+    stickyTop();
+    initMenuRef("#header .headLogo", "#splashPage");
+    initMenuRef("#aboutNav", "#aboutPage");
+    initMenuRef("#timelineNav", "#timelinePage");
+});
+
+$window.resize(function() {
+    stickyTop();
+});
+$window.scroll(function() {
+    stickyTop();
+});
+
+
